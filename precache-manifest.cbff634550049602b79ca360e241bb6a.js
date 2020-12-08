f4281a4ba12bd9def69ed5d8ec54b2dedf28a31d
Alisha-bashir <bussinessmail1257@gmail.com>
2020-12-08 07:35:51 -0800

    Updates

---
+++ precache-manifest.cbff634550049602b79ca360e241bb6a.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd4b1d7214f64774005588c4fc969a93",
+    "url": "/Alisha-bashir.github.io/portfolio/index.html"
+  },
+  {
+    "revision": "e21366442b72054f53bc",
+    "url": "/Alisha-bashir.github.io/portfolio/static/css/3.879e9400.chunk.css"
+  },
+  {
+    "revision": "92d7c1bda9c69be6cfe5",
+    "url": "/Alisha-bashir.github.io/portfolio/static/css/4.54df59d4.chunk.css"
+  },
+  {
+    "revision": "580bb5099dbc95b06513",
+    "url": "/Alisha-bashir.github.io/portfolio/static/css/main.2e3ed775.chunk.css"
+  },
+  {
+    "revision": "ee3d7d41bef058d96cfd",
+    "url": "/Alisha-bashir.github.io/portfolio/static/js/2.bf1b0ea6.chunk.js"
+  },
+  {
+    "revision": "e21366442b72054f53bc",
+    "url": "/Alisha-bashir.github.io/portfolio/static/js/3.bfe6926a.chunk.js"
+  },
+  {
+    "revision": "92d7c1bda9c69be6cfe5",
+    "url": "/Alisha-bashir.github.io/portfolio/static/js/4.ee8f81f8.chunk.js"
+  },
+  {
+    "revision": "580bb5099dbc95b06513",
+    "url": "/Alisha-bashir.github.io/portfolio/static/js/main.eb13f27a.chunk.js"
+  },
+  {
+    "revision": "99462f7b4e525f443fc7",
+    "url": "/Alisha-bashir.github.io/portfolio/static/js/runtime-main.c2517132.js"
+  },
+  {
+    "revision": "c83a3a8a0f6b5f5082f48c983f33f4ab",
+    "url": "/Alisha-bashir.github.io/portfolio/static/media/0.c83a3a8a.png"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/Alisha-bashir.github.io/portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/Alisha-bashir.github.io/portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/Alisha-bashir.github.io/portfolio/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/Alisha-bashir.github.io/portfolio/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/Alisha-bashir.github.io/portfolio/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/Alisha-bashir.github.io/portfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/Alisha-bashir.github.io/portfolio/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "3068fa970286cee65ba62d19ed5af72a",
+    "url": "/Alisha-bashir.github.io/portfolio/static/media/humanrolodex.3068fa97.PNG"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/Alisha-bashir.github.io/portfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/Alisha-bashir.github.io/portfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/Alisha-bashir.github.io/portfolio/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "44fe07f17debbdcf10c2d270afaee9f5",
+    "url": "/Alisha-bashir.github.io/portfolio/static/media/ztmlogo.44fe07f1.png"
+  }
+]);
